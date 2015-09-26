0b377fc0ecef6fce2fa286bc9e779170d044aa06
Nop287 <norbert.widmann@gmx.de>
2015-09-26 23:03:58 +0200

    Corrected Version

--- server.R
+++ server.R
@@ -1,25 +1,28 @@
-library(UsingR)
-data(galton)
+library(shiny)
+library(ggplot2)
+library(WDI)
 
-## Loading World Bank data for GDP of european countries
+# Loading World Bank data for GDP of european countries
 dat = WDI(indicator="NY.GDP.MKTP.CD", country=c("BE", "BG", "CZ", "DK", "DE", "EE", "IE", "GR", "ES", "FR", "HR", "IT", "CY", "LV", "LT", "LU", "HU", "MT", "NL", "AT", "PL", "PT", "RO", "SI", "SK", "FI", "SE", "GB"), start=1945, end=2015)
+# Convert the relevant fields to factor
 dat$country = factor(dat$country)
 dat$iso2c = factor(dat$iso2c)
 
-## Plotting the data
-ggplot(dat, aes(year, NY.GDP.PCAP.KD, color=country)) + geom_line() + 
-  xlab('Year') + ylab('GDP per capita')
-
-shinyServer(
-  function(input, output) {
-    output$newHist <- renderPlot({
-      hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-      mu <- input$mu
-      lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-      mse <- mean((galton$child - mu)^2)
-      text(63, 150, paste("mu = ", mu))
-      text(63, 140, paste("MSE = ", round(mse, 2)))
-    })
-    
-  }
-)
+# Define server logic required to draw a plot of yearly GDP
+shinyServer(function(input, output) {
+  
+  # Expression that generates a plot of GDP. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should be automatically
+  #     re-executed when inputs change
+  #  2) Its output type is a plot
+  
+  output$gdpPlot <- renderPlot({
+    plotDat <- dat[dat$country %in% input$checkboxCountry, ]
+    plotDat <- plotDat[plotDat$year >= input$sliderYear[1] & plotDat$year <= input$sliderYear[2], ]
+    ggplot(plotDat, aes(year, NY.GDP.MKTP.CD, color=country)) + geom_line() + 
+                xlab('Year') + ylab('GDP per capita')
+  })
+  
+})
--- ui.R
+++ ui.R
@@ -1,9 +1,27 @@
-shinyUI(pageWithSidebar(
-  headerPanel("Example plot"),
-  sidebarPanel(
-    sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)
-  ),
-  mainPanel(
-    plotOutput('newHist')
+library(shiny)
+
+# Define UI for application that draws a plot of GDP from World Bank data
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Yearly GDP of European Countries (US$)"),
+  
+  # Sidebar with a slider input for year from/to and 
+  # a checkbox group for country
+  sidebarLayout(
+    sidebarPanel(
+      # Slider for the year from/to
+      sliderInput("sliderYear", label = h3("Year from/to"), min = min(dat$year), 
+                  max = max(dat$year), sep = "", value = c(min(dat$year), max(dat$year))),
+      # checkbox group for country
+      checkboxGroupInput("checkboxCountry", "Choose a country:", 
+                         choices = levels(dat$country), selected = "Germany"
+      )
+    ),
+    
+    # Show the plot
+    mainPanel(
+      plotOutput("gdpPlot")
+    )
   )
 ))
